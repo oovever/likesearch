32e69ca641a0df7a0f04bac6b97078da9988d45b
rocmu <rocmu@tencent.com>
2023-05-11 10:33:12 +0800

    add dependency

--- src/main/java/org/likeserch/LikeSearch.java
+++ src/main/java/org/likeserch/LikeSearch.java
@@ -1,6 +1,7 @@
 package org.likeserch;
+import org.apache.commons.lang3.StringUtils;
 
-
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
